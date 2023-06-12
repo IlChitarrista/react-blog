e9bb4d6b3cd3162e31389941cd0b9832c7f3ace9
Mattia Formichetti <mattiaformichetti@gmail.com>
2023-06-12 10:52:38 +0200

    Initial ReactMarkdown demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+const Title = ({ children }) => {
+  return <h1 className="text-center">{children}</h1>;
+};
+
+const Em = ({ children }) => {
+  return <em className="text-red-500">{children}</em>;
+};
 
 function App() {
-  return <div className="App"></div>;
+  const markdown = `# Hello There *General Kenoby*`;
+  return (
+    <div>
+      <ReactMarkdown components={{ h1: Title, em: Em }}>
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  );
 }
 
 export default App;
